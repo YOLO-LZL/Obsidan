abab3b865df167e459b29487d2241d133198c1b1
lzl <1077660576@qq.com>
2026-07-24 22:41:25 +0800

    1

--- .obsidian/plugins/claudian-2.0.2/src/core/providers/modelRouting.ts
+++
@@ -1,6 +0,0 @@
-import { ProviderRegistry } from './ProviderRegistry';
-import type { ProviderId } from './types';
-
-export function getProviderForModel(model: string, settings?: Record<string, unknown>): ProviderId {
-  return ProviderRegistry.resolveProviderForModel(model, settings);
-}
--- .obsidian/plugins/obsidian-easy-tracker-1.0.4/entry-types.ts
+++
@@ -1,4 +0,0 @@
-export interface Entry {
-    date: Date;
-    value: number;
-}
